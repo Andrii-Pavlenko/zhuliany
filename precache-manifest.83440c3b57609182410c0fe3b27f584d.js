48524858ca4e7516c359db4c972b6f4e99caa136
Andrii <farikpite@gmail.com>
2020-02-19 21:48:34 +0200

    Updates

---
+++ precache-manifest.83440c3b57609182410c0fe3b27f584d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2c26a128ee952cd903ca68f06f1f84",
+    "url": "/zhuliany/index.html"
+  },
+  {
+    "revision": "0c860b9e97de0228330b",
+    "url": "/zhuliany/static/css/main.cea54700.chunk.css"
+  },
+  {
+    "revision": "58f65b02b3588d89fb86",
+    "url": "/zhuliany/static/js/2.50fd4986.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/zhuliany/static/js/2.50fd4986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c860b9e97de0228330b",
+    "url": "/zhuliany/static/js/main.c3347d34.chunk.js"
+  },
+  {
+    "revision": "04922fd9ccded2d30ecd",
+    "url": "/zhuliany/static/js/runtime-main.9e568839.js"
+  }
+]);
